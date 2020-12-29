fee75488e65d1af0e5503a2deb639dbfc03571d0
mhmdhalawi <mhmdhalawi@users.noreply.github.com>
2020-12-29 22:22:41 +0200

    structure change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import { DataComponent } from './context/context';
 import MainContext from './context/mainContext';
 
 ReactDOM.render(
